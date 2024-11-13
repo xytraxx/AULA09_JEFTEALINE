b4931aae46fed365e2cfc1fa3d73a25a7c7f72fa
XytraX <alineruizdias@gmail.com>
2024-11-13 08:20:35 -0300

    Add files via upload

---
+++ AULA09_JS_TEMPLATE_STRINGS_ALINE_JEFTE/script.js
@@ -0,0 +1,16 @@
+let nome = "Aline";
+let sobreNome = "Jefte";
+let idade = 17;
+
+let mensagem =
+  "Meu nome é " +
+  nome +
+  " " +
+  sobreNome +
+  " e minha idade é " +
+  idade +
+  " anos. ";
+console.log(mensagem);
+
+let mensagem2 = `Meu nome é ${nome} ${sobreNome} e minha idade é ${idade} anos.`;
+console.log(mensagem2);
